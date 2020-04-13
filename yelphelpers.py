af039c7e05c92d73504e302481831af75adc355c
Justin Lam <justinmlam@gmail.com>
2020-04-13 15:47:21 -0700

    Added categorical statistics

--- yelphelpers.py
+++ yelphelpers.py
@@ -12,3 +12,16 @@ def expand_list(df,lst_col):
       for col in df.columns.drop(lst_col)}
     ).assign(**{lst_col:np.concatenate(df[lst_col].values)})[df.columns]
     return df
+
+
+def bayes_avg(df,cond,col):
+    """
+    This function was taken from Heidi Jiang's "New Porkers" project
+    See: https://github.com/heidijiang/new-porkers/
+    
+    """
+    v = df.groupby(cond)[col].size()
+    m = df[cond].value_counts().mean()
+    w = v/(v+m)
+    S = w*df.groupby(cond)[col].mean() + (1-w)*df[col].mean()
+    return S
